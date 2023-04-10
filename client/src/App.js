3d992b6527b11edea4fb23c38a25d9dee989637d
0xnunana <kudayapaul@gmail.com>
2023-04-10 15:14:27 +0000

    cra files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Background from "./components/Background";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-blue-200 relative">
+      <div className="sticky top-0"><Header /></div>
+      
+      <Background />
+      <Gallery/>
+      <Footer/>
+      
+      
     </div>
   );
 }

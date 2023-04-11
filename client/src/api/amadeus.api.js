0969a59fc5aab20b0dbb31f20b548ef2984021c6
0xnunana <kudayapaul@gmail.com>
2023-04-11 09:15:45 +0000

    api test

--- client/src/api/amadeus.api.js
+++ client/src/api/amadeus.api.js
@@ -42,11 +42,53 @@ const search = (input) => {
     cancel() {},
   };
 };
+// const flight = ({first,second,depart}) => {
+//     if () {
+//       try {
+//         const source = CancelToken.source();
+//         const request = axios.get(`/api/search?keyword=${input}`, {
+//           cancelToken: source.token,
+//         });
+//         return {
+//           async process(callback) {
+//             request.then((response) => {
+//               const json = response.data;
+  
+//               if (json && json.data) {
+//                 callback(
+//                   json.data.map(({ address }) => {
+//                     return {
+//                       city: address.cityName,
+//                       code: address.cityCode,
+//                       country: address.countryName,
+//                       state: address.stateCode,
+//                     };
+//                   })
+//                 );
+//               }
+//             });
+//           },
+//           cancel() {
+//             source.cancel();
+//           },
+//         };
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//     return {
+//       process() {
+//         return [];
+//       },
+//       cancel() {},
+//     };
+//   };
 
-const flight = async(dept,arr,deptDate,child,adults)=>{
+const flight = async({first,second,depart})=>{
+    
   try{
     const request= await axios.get(
-      `api/shopping/flight-offers?originLocationCode=${dept}&destinationLocationCode=${arr}&departureDate=${deptDate}&adults=${adults}&children=${child}`,
+    `api/shopping/flight-offers?originLocationCode=${first}&destinationLocationCode=${second}&departureDate=${depart}&adults=1&max=5`,
       
     );
     const json = request.data;
